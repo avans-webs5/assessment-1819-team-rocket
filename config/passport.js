e7851c069752a8f5ed23bfb1bc8291dc893c99a8
Storm <stormwitziers123@gmail.com>
2020-03-23 13:06:06 +0100

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -17,12 +17,12 @@ module.exports = function(passport){
     // =========================================================================
 
     passport.serializeUser(function(user, done){
-        done(null, user.id);
+        done(null, user.user.id);
     });
 
     passport.deserializeUser(function(id, done){
         User.findById(id, function(err, user){
-            done(err, user);
+            done(err, user.user);
         });
     });
 
