dc3e44f31f9f4a8c7c1c7678dbc26e9019a05b3a
Storm <stormwitziers123@gmail.com>
2020-07-21 00:41:38 +0200

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 
     facebookAuth : {
         'clientID'      : '133906824688190', 
-        'clientSecret'  : 'd9afbbcc27066da11579320e7369bbe2', 
+        'clientSecret'  : 'SECRET', 
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] 
@@ -10,7 +10,7 @@ module.exports = {
 
     googleAuth : {
         'clientID'      : '176114730172-vj827q3bdreo342ac3gcqad2ba1pdlch.apps.googleusercontent.com',
-        'clientSecret'  : 'LKY8J2l5lj90ClyAvlnSC4FQ',
+        'clientSecret'  : 'SECRET',
         'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     },
     JWS: {
